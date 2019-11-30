8d30772576a1a3814b2e7d39de0a4e3b80354e4d
Brandon <brandy@scarecrowarts.com>
2019-11-30 18:15:33 -0500

    blank api

---
+++ source/APIStuff.hx
@@ -0,0 +1,14 @@
+package;
+
+/**
+ * ...
+ * @author NInjaMuffin99
+ */
+class APIStuff 
+{
+
+    public static var APIID:String = "";
+    public static var EncKey:String = "";
+	public static var Session:String;
+    
+}
