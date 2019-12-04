fa3e22642dbf0b917aed774f87e49d36d0351e25
George FunBook <gkurelic@gmail.com>
2019-12-04 17:14:54 -0600

    disable scroll

--- source/Main.hx
+++ source/Main.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxGame;
 import openfl.Assets;
 import openfl.display.Sprite;
+import openfl.events.MouseEvent;
 
 import states.*;
 
@@ -14,5 +15,6 @@ class Main extends Sprite
 		addChild(new FlxGame(320, 180, IntroState, 1, 60, 60, true));
 		
 		//stage.showDefaultContextMenu = false;
+		stage.addEventListener(MouseEvent.MOUSE_WHEEL, (e)->e.preventDefault());
 	}
 }
