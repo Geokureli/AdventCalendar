dc81a0067dfbb428798427d3e038663dfbc9a12c
George FunBook <gkurelic@gmail.com>
2019-12-21 14:12:31 -0600

    sound stuff

--- source/data/Instrument.hx
+++ source/data/Instrument.hx
@@ -120,7 +120,7 @@ class Instrument
                 volume = 1.0;
             case Piano:
                 soundPath = PATH + "piano/";
-                sustainMode = true;
+                // sustainMode = true;
                 volume = 1.0;
         }
         
